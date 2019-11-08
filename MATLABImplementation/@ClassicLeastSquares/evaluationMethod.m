db18f883ffa1c233379b9642b6e6f78566537575
Evangelos Katsavrias <vageng@gmail.com>
2019-11-08 01:50:09 +0100

    license note

--- MATLABImplementation/@ClassicLeastSquares/evaluationMethod.m
+++ MATLABImplementation/@ClassicLeastSquares/evaluationMethod.m
@@ -1,3 +1,26 @@
+%%  CUDA_MLS Framework
+%
+%   Copyright 2017-2018 Evangelos D. Katsavrias, Luxembourg
+%
+%   This file is part of the CUDA_MLS Framework.
+%
+%   CUDA_MLS Framework is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License version 3 as published by
+%   the Free Software Foundation.
+%
+%   CUDA_MLS Framework is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with CUDA_MLS Framework.  If not, see <https://www.gnu.org/licenses/>.
+%
+%   Contact Info:
+%   Evangelos D. Katsavrias
+%   email/skype: vageng@gmail.com
+% -----------------------------------------------------------------------
+
 function evaluationMethod(obj)
 
 obj.m_evaluations = evaluatePoints_ClassicLeastSquares(obj.m_evaluationPoints, @(x)obj.m_polynomial.evaluate(x), obj.m_coefficients);
--- cpp_cuda/include/Math/cuBLAS_wrappers.h
+++ cpp_cuda/include/Math/cuBLAS_wrappers.h
@@ -1,3 +1,26 @@
+//   CUDA_MLS Framework
+//
+//   Copyright 2017-2018 Evangelos D. Katsavrias, Luxembourg
+//
+//   This file is part of the CUDA_MLS Framework.
+//
+//   CUDA_MLS Framework is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 3 as published by
+//   the Free Software Foundation.
+//
+//   CUDA_MLS Framework is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with CUDA_MLS Framework.  If not, see <https://www.gnu.org/licenses/>.
+//
+//   Contact Info:
+//   Evangelos D. Katsavrias
+//   email/skype: vageng@gmail.com
+// -----------------------------------------------------------------------
+
 __host__ __device__
 inline cublasStatus_t cublasGgemv(cublasHandle_t handle, cublasOperation_t trans, int m, int n, const float *alpha, const float *A, int lda, const float *x, int incx, const float *beta, float *y, int incy) {
 	return cublasSgemv(handle, trans, m, n, alpha, A, lda, x, incx, beta, y, incy); }
